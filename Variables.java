ceff316fafc91beafeea70839c649860b44467c1
Steve Caffrey <stephen.caffrey@travisperkins.co.uk>
2016-02-24 16:49:19 +0000

    Exercise 2: Variables

---
+++ Variables.java
@@ -0,0 +1,9 @@
+public class Variables {
+
+    public static void main(String[] args) {
+        int num1 = 8;
+        int num2 = 6;
+        int result = num1 + num2;
+        System.out.println("The result is " + result);
+    }
+}
