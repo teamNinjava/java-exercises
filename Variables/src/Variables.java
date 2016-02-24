121570d2cda9a8d6b079de17a444df91a0e6312a
Maryam Jeiran <maryam.jeiran@travisperkins.co.uk>
2016-02-24 09:11:09 +0000

    variables

---
+++ Variables/src/Variables.java
@@ -0,0 +1,11 @@
+
+public class Variables {
+	
+	public static void main(String[] args){
+		int num1 = 3;
+		int num2 = 2;
+		int result = num1 + num2 ;
+		System.out.println("result: " + result);
+	}
+
+}
