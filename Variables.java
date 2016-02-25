3c0e0acb37e92cfd30659e0b9fe2671c8e210f33
Stephen Clarke <stephen.clarke@travisperkins.co.uk>
2016-02-25 09:35:11 +0000

    Create Variables class

---
+++ Variables.java
@@ -0,0 +1,10 @@
+public class Variables {
+
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2 = 2;
+        int result = num1 + num2;
+        System.out.println("Result = " + result);
+    }
+
+}
