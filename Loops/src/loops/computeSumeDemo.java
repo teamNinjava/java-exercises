70535496f28d272578985ed87894f36c0aa28466
Maryam Jeiran <maryam.jeiran@travisperkins.co.uk>
2016-04-18 12:40:39 +0100

    loop exercise

---
+++ Loops/src/loops/computeSumeDemo.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class computeSumeDemo {
+
+	public static void main(String[] args){
+		computeSum mySum = new computeSum();
+		
+		mySum.calculate(4);
+		System.out.println(mySum.sum);
+		
+	}
+
+}
